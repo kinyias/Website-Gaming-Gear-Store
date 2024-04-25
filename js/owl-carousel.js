306a18295a0815858db0299a58256c335887a2a2
Thai Tin Khang <kinyiasgit@gmail.com>
2024-04-25 20:59:04 +0700

    update footer and blog

--- js/owl-carousel.js
+++ js/owl-carousel.js
@@ -21,16 +21,40 @@ $(document).ready(function () {
       '<i class="bi bi-arrow-left"></i>',
       '<i class="bi bi-arrow-right"></i>',
     ],
-    navContainer: '.best-seller-title .custom-nav',
+    navContainer: '.box-title .custom-nav-best-seller',
     responsive: {
       0: {
-        items: 4,
+        items: 2,
       },
       600: {
-        items: 1,
+        items: 4,
       },
       1000: {
         items: 4,
+      },
+    },
+  });
+  $('.featured-categories-list').owlCarousel({
+    loop: true,
+    margin: 30,
+    singleItem: true,
+    items: 3,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    navContainer: '.box-title .custom-nav-featured-categories',
+    responsive: {
+      0: {
+        items: 2,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
       },
     },
   });
